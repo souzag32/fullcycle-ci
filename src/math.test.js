e7d85c99f16712b26147b1cf42380d34964aa9ac
Gabriel <souzag32@gmail.com>
2024-04-14 18:53:30 +0000

    ci: run ci with error

--- src/math.test.js
+++ src/math.test.js
@@ -4,14 +4,14 @@ test("sum (1, 2) to be equal 3", () => {
     expect(sum(1, 2)).toBe(3);
 });
 
-test("sub (1, 2) to be equal -1", () => {
-    expect(sub(1, 2)).toBe(-1);
-});
+// test("sub (1, 2) to be equal -1", () => {
+//     expect(sub(1, 2)).toBe(-1);
+// });
 
-test("times (2, 2) to be equal 4", () => {
-    expect(times(2, 2)).toBe(4);
-});
+// test("times (2, 2) to be equal 4", () => {
+//     expect(times(2, 2)).toBe(4);
+// });
 
-test("pow(2, 3) to be equal 8", () => {
-    expect(pow(2, 3)).toBe(8);
-});
+// test("pow(2, 3) to be equal 8", () => {
+//     expect(pow(2, 3)).toBe(8);
+// });
