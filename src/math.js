6c7c4287621140606958b19f4650ae2c09fa109e
Gabriel <souzag32@gmail.com>
2024-04-14 16:58:21 +0000

    feat: add math functions

---
+++ src/math.js
@@ -0,0 +1,19 @@
+function sum(a, b) {
+    return a + b;
+}
+
+function sub(a, b){
+    return a - b;
+}
+
+function times(a, b){
+    return a * b;
+}
+
+function pow(a, b){
+    return Math.pow(a, b);
+}
+
+module.exports = {
+    sum, sub, times, pow
+}
